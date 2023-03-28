2c55a36707310b590541c0d6ae26f0ee3e1ba1af
Ikenna <ikenna@kali.Ikenna-PC>
2023-03-28 22:22:02 +0100

    Map data structure , More map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+function groceriesList() {
+  const newMap = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  return new Map(newMap);
+}
+
+module.exports = groceriesList;
